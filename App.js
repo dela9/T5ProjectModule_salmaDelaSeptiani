84c387f59e197f97b288fbac5b1d115ba660602b
dela9 <124057172+dela9@users.noreply.github.com>
2023-11-04 21:28:41 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+const fullName= require('./module1');
+const myAge= require('./module2');
+const myHobby= require('./module3');
+const myWork= require('./module4');
+
+console.log(fullName('Salma dela septiani'));
+console.log(myAge('20 tahun'));
+console.log(myHobby('bernyanyi,travel,berenang'));
+console.log(myWork('dokter'))
